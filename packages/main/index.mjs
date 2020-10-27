0790c5e768d1dbc8aec3f6c3cc5451d0bd513348
Paus <tpr.paus@gmail.com>
2020-10-27 15:24:06 +0200

    added connection to db + configured lerna

--- packages/main/index.mjs
+++ packages/main/index.mjs
@@ -1,6 +1,8 @@
 import express from 'express'
 import {} from 'dotenv/config.js'
 
+import { connection } from '../db/connection.mjs'
+
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -8,5 +10,7 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+console.log(connection.name)
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
